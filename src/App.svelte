a0fe3af09d03701ae3cd195b7f40c4fe0391735c
buntafujikawa <bunta.fujikawa@gmail.com>
2020-05-30 21:02:14 +0900

    add sample animations using lottie files.
    add readme
    
    fix indent

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,53 @@
 <script>
-	export let name;
+
+  export let name;
+
+  import lottie from "lottie-web";
+  import { onMount } from "svelte"
+
+  onMount(() => {
+    const loading = lottie.loadAnimation({
+      name: 'loading',
+      container: document.getElementById('loading'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: './animations/22189-progress.json'
+    });
+  });
+
+  function like() {
+    lottie.loadAnimation({
+      name: 'like',
+      container: document.getElementById('like'),
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: './animations/899-like.json'
+    });
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <div id="loading"></div>
+
+  <div id="like"></div>
+  <button on:click="{like}">Like</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 </style>
